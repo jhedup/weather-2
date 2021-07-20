0bd132860ba3b74a0120cce0f8920cd23db4e883
jhedup <jheduplessis@gmail.com>
2021-07-20 22:47:42 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
+bluetooth.onBluetoothConnected(function () {
+    bluetooth.startTemperatureService()
+})
+bluetooth.onBluetoothDisconnected(function () {
+	
+})
 weatherbit.startWeatherMonitoring()
 basic.forever(function () {
     basic.showNumber(weatherbit.temperature() / 100)
